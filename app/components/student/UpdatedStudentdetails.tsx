8582a5bb94afc3aa5c1be7bb80c3a562aba6dca0
puspita <puspitabanik213@gmail.com>
2025-12-03 23:00:49 +0600

    new updated

--- app/components/student/UpdatedStudentdetails.tsx
+++ app/components/student/UpdatedStudentdetails.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import { useStudents } from "./StudentContext";
 import { img6 } from "~/utils/images/temp.image"; 
 
-export default function UpdatedStudentdetails() {
+export default function UpdatedStudentDetails() {
   const { id } = useParams();
   const { students } = useStudents();
 
